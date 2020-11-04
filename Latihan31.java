f4b461fb20b2a912712cbd4bcf563469758a23e3
Muhammad Fauzan <muhammadfauzanfadhlulbarr@gmail.com>
2020-11-04 17:56:42 +0700

    first commit

---
+++ Latihan31.java
@@ -0,0 +1,18 @@
+/**
+ * Latihan31
+ * Nama     : Muhammad Fauzan Fadhlulbarr
+ * Kelas    : PBO10K
+ * NIM      : 10119176
+ * Program  : Perkenalan Mahasiswa
+ */
+class mahasiswa {
+    public String nim;
+    public String nama;
+    
+   public void PerkenalanDiri(String nim, String nama){
+       System.out.println("Hello Everyone");
+       System.out.println("My NIM is "  + nim);
+       System.out.println("My Name is "  + nama);
+   }
+    
+}
---
+++ PBO10k10119176Latihan31.java
@@ -0,0 +1,31 @@
+/**
+ * Latihan31
+ * Nama     : Muhammad Fauzan Fadhlulbarr
+ * Kelas    : PBO10K
+ * NIM      : 10119176
+ * Program  : Perkenalan Mahasiswa
+ */
+public class PBO10k10119176Latihan31 {
+
+    public static void main(String[] args) {
+      mahasiswa mahasiswa1 = new mahasiswa();
+      mahasiswa1.nim = "10110269";
+      mahasiswa1.nama="Rizki Adam Kurniawan"+"\n";
+      mahasiswa1.PerkenalanDiri(mahasiswa1.nim, mahasiswa1.nama);
+      
+      mahasiswa mahasiswa2 = new mahasiswa();
+      mahasiswa2.nim = "10110270";
+      mahasiswa2.nama="Indra Tiola"+"\n";
+      mahasiswa2.PerkenalanDiri(mahasiswa2.nim, mahasiswa2.nama);
+      
+      mahasiswa mahasiswa3 = new mahasiswa();
+      mahasiswa3.nim = "10110271";
+      mahasiswa3.nama= "Robi Tanzil Ganefi"+ "\n";
+      mahasiswa3.PerkenalanDiri(mahasiswa3.nim, mahasiswa3.nama);
+      
+      mahasiswa mahasiswa4 = new mahasiswa();
+      mahasiswa4.nim = "10110272";
+      mahasiswa4.nama="muhammad Nur Awaludin"+"\n";
+      mahasiswa4.PerkenalanDiri(mahasiswa4.nim, mahasiswa4.nama);
+  }
+}
